83eb8b38cb137fe5d000dd9b110caf38f3595615
mkbx06 <152910138+mkbx06@users.noreply.github.com>
2023-12-05 09:52:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,11 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+1(831)304-5385', 'MKBX', true],
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "" //+1(831)-5385
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
@@ -50,17 +49,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.botname = 'ᴛʜᴇ ᴍᴋʙx-ʙᴏᴛ'
+global.premium = 'false'
+global.packname = 'ᴛʜᴇ ᴍᴋʙx-ʙᴏᴛ' 
+global.author = 'mikibix' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/fvck_mikibixx\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.fgsc = 'https://github.com/Guru322/GURU-BOT'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
